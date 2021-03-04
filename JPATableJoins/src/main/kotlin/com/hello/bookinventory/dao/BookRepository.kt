3ddf5f355af9d02b973cce826b4b2f17b5a7be62
Ajay <writeajay@gmail.com>
2021-03-04 00:31:54 -0800

    Removed reference to deleted entry Product

--- JPATableJoins/src/main/kotlin/com/hello/bookinventory/dao/BookRepository.kt
+++ JPATableJoins/src/main/kotlin/com/hello/bookinventory/dao/BookRepository.kt
@@ -1,6 +1,5 @@
 package com.hello.bookinventory.dao
 
-import com.hello.bookinventory.entities.Product
 import com.hello.bookinventory.model.Book
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
